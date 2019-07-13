db0caed8270469e973f79e722c9b21ad88681d3e
Srinath R <r.srinath@live.com>
2019-07-13 15:13:36 +0530

    Initial commit

---
+++ src/main/java/io/chalkslate/Hello.java
@@ -0,0 +1,9 @@
+package io.chalkslate;
+
+public class Hello {
+
+    public void sayHello(String name) {
+        System.out.println("Hi " + name);
+    }
+
+}
---
+++ src/test/java/io/chalkslate/HelloTest.java
@@ -0,0 +1,11 @@
+package io.chalkslate;
+
+import org.junit.Test;
+
+public class HelloTest {
+
+    @Test
+    public void testHello() {
+        new Hello().sayHello("Srinath");
+    }
+}
