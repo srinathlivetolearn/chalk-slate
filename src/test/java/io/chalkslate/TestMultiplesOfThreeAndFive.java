f6fe8ba2e2d43a0d41d4945f69ec7987c4138d96
Srinath R <r.srinath@live.com>
2019-07-13 15:44:20 +0530

    Problem 1

---
+++ src/test/java/io/chalkslate/TestMultiplesOfThreeAndFive.java
@@ -0,0 +1,13 @@
+package io.chalkslate;
+
+import org.junit.Test;
+
+public class TestMultiplesOfThreeAndFive {
+
+    private MultiplesOfThreeAndFive multiples = new MultiplesOfThreeAndFive();
+
+    @Test
+    public void testMultiplesSum() {
+        System.out.println(multiples.findSumOfMultiples(1000));
+    }
+}
