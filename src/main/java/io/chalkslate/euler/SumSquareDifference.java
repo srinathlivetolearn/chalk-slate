0d489342ab99657b96057d239b8526795989d7a4
Srinath R <r.srinath@live.com>
2019-08-03 15:19:17 +0530

    Solved problem 6 of euler project.
    Signed-off-by: Srinath R <r.srinath@live.com>

---
+++ src/main/java/io/chalkslate/euler/SumSquareDifference.java
@@ -0,0 +1,38 @@
+package io.chalkslate.euler;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.stream.LongStream;
+
+/**
+ * The sum of the squares of the first ten natural numbers is,
+ * <p>
+ * 12 + 22 + ... + 102 = 385
+ * The square of the sum of the first ten natural numbers is,
+ * <p>
+ * (1 + 2 + ... + 10)2 = 552 = 3025
+ * Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+ * <p>
+ * Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+ * <p>
+ * https://projecteuler.net/problem=6
+ */
+public class SumSquareDifference {
+    private long sumOfSquare = 0L;
+
+    private long squareOfSum = 0L;
+
+    public long compute(int n) {
+        Executor executor = Executors.newFixedThreadPool(2);
+        CompletableFuture<Void> sumSquares = CompletableFuture.runAsync(() ->
+                this.sumOfSquare = LongStream.range(1, n + 1).map(i -> i * i).sum(), executor);
+        CompletableFuture<Void> squareSum = CompletableFuture.runAsync(() -> {
+            long sum = LongStream.range(1, n + 1).sum();
+            this.squareOfSum = sum * sum;
+        }, executor);
+        CompletableFuture.allOf(sumSquares, squareSum).join();
+        return squareOfSum - sumOfSquare;
+    }
+
+}
---
+++ src/test/java/io/chalkslate/euler/TestSumSquareDifference.java
@@ -0,0 +1,14 @@
+package io.chalkslate.euler;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestSumSquareDifference {
+
+    @Test
+    public void testCompute() {
+        long result = new SumSquareDifference().compute(100);
+        assertEquals(25164150L, result);
+    }
+}
