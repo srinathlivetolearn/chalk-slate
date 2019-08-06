9fb019688793c0443fde9dcf0cdd88f6af53a5f8
Srinath R <srinath.ravindranath@daimler.com>
2019-08-06 17:54:10 +0530

    Optimisation of problem1.

--- src/test/java/io/chalkslate/euler/TestMultiplesOfThreeAndFive.java
+++ src/test/java/io/chalkslate/euler/TestMultiplesOfThreeAndFive.java
@@ -1,5 +1,7 @@
 package io.chalkslate.euler;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class TestMultiplesOfThreeAndFive {
@@ -8,6 +10,13 @@ public class TestMultiplesOfThreeAndFive {
 
     @Test
     public void testMultiplesSum() {
-        System.out.println(multiples.findSumOfMultiples(1000));
+       assertEquals(233168L, multiples.findSumOfMultiples(1000));
+    }
+    
+    @Test
+    public void testOptimalSolution() {
+    	long sum = multiples.findSumOfMultiplesUsingAP(1000);
+		System.out.println(sum);
+    	assertEquals(233168L, sum);
     }
 }
