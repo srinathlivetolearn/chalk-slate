690b8ba38aadea1949deb8ec7f0efdafa5f46136
Srinath R <r.srinath@live.com>
2019-08-24 20:11:17 +0530

    Improve code coverage.

--- src/test/java/io/chalkslate/euler/TestLargestPrimeFactor.java
+++ src/test/java/io/chalkslate/euler/TestLargestPrimeFactor.java
@@ -5,9 +5,15 @@ import org.junit.Test;
 
 public class TestLargestPrimeFactor {
 
-	@Test
-	public void testComputeLargestPrimeFactor() {
-		long largestPrimeFactor = new LargestPrimeFactor().computeLargestPrimeFactor(600851475143L);
-		Assert.assertEquals(6857L,largestPrimeFactor);
-	}
+    @Test
+    public void testComputeLargestPrimeFactor() {
+        long largestPrimeFactor = new LargestPrimeFactor().computeLargestPrimeFactor(600851475143L);
+        Assert.assertEquals(6857L, largestPrimeFactor);
+    }
+
+    @Test
+    public void testComputeWithEvenInput() {
+		long primeFactor = new LargestPrimeFactor().computeLargestPrimeFactor(108L);
+		Assert.assertEquals(3L,primeFactor);
+    }
 }
